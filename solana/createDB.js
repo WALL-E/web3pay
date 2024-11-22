f5c5c8dcb677693cc82c3bd4f7c8a36020847339
Ubuntu <ubuntu@ip-172-31-8-246.eu-north-1.compute.internal>
2024-11-22 14:43:58 +0000

    update createDB.js

---
+++ solana/createDB.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const sqlite3 = require('sqlite3').verbose()
+const fs = require('fs').promises;
+const path = require('path');
+
+const SQLITEDB = process.env.SQLITEDB;
+
+// 运行函数
+(async () => {
+    try {
+        var db = new sqlite3.Database(
+            SQLITEDB,
+            sqlite3.OPEN_READWRITE,
+            function (err) {
+                if (err) {
+                    return console.log(err.message)
+                }
+                console.log('connect database successfully')
+            }
+        )
+        
+	const sql = `CREATE TABLE wallet (
+	        owner TEXT NOT NULL PRIMARY KEY,
+		        public_key TEXT NOT NULL,
+		        private_key TEXT NOT NULL,
+		        create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	)`
+        db.run(sql, function (err) {
+            if (err) {
+                  return console.log(err)
+            }
+            console.log('create table wallet')
+        })
+
+
+        db.close(function (err) {
+            if (err) {
+                    return console.log(err.message)
+            }
+            console.log('close database connection')
+        })
+
+    } catch (error) {
+        console.error('重置数据库时发生错误:', error);
+    }
+})();
--- solana/resetDB.js
+++ solana/resetDB.js
@@ -1,18 +1,22 @@
+require('dotenv').config();
+
 const sqlite3 = require('sqlite3').verbose()
 const fs = require('fs').promises;
 const path = require('path');
 
+const SQLITEDB = process.env.SQLITEDB;
+
 async function resetDatabase() {
-    const dbPath = path.join(__dirname, 'pay.db');
+    const dbPath = path.join(__dirname, SQLITEDB);
 
     try {
         // 检查文件是否存在
         await fs.access(dbPath);
-        console.log('找到现有的 pay.db 文件');
+        console.log(`找到现有的 ${SQLITEDB} 文件`);
 
         // 文件存在，删除它
         await fs.unlink(dbPath);
-        console.log('已删除现有的 pay.db 文件');
+        console.log(`已删除现有的 ${SQLITEDB} 文件`);
     } catch (error) {
         // 文件不存在，忽略错误
         if (error.code !== 'ENOENT') {
@@ -24,7 +28,7 @@ async function resetDatabase() {
     try {
         // 创建新的空文件
         await fs.writeFile(dbPath, '');
-        console.log('已创建新的 pay.db 文件');
+        console.log(`已创建新的${SQLITEDB} 文件`);
     } catch (error) {
         console.error('创建新文件时出错:', error.message);
     }
